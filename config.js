9cf444e66ccbefb8c55d1c58a6c4c4039a10bcd3
Juan Gonzalez <juanj.gonzalezp@gmail.com>
2016-01-15 11:00:50 -0430

    Editing config.js file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	API_ENDPOINT: "https://api.bufferapp.com/1",
 	ACCESS_TOKEN: process.env.ACCESS_TOKEN,
-	TWITTER_ID: process.env.TWITTER_ID,
+	TWITTER_PID: process.env.TWITTER_ID,
 	FACEBOOK_PID: process.env.FACEBOOK_PID,
 	LINKEDIN_PID: process.env.LINKEDIN_PID,
 }
